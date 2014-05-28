7437c0031917fc4243042d60040c238e8c938a80
gouthamkreddy <gouthamkreddy@gmail.com>
2014-05-28 19:16:43 +0530

    Create test.js

---
+++ demo/test.js
@@ -0,0 +1,43 @@
+var nodemailer = require('nodemailer');
+
+var transport = nodemailer.createTransport("SMTP", {
+      host: "smtp.163.com",
+      port: 25,
+      auth: {
+            user: "greddy@163.com",
+            pass: "123"
+      }
+});
+
+console.log('SMTP configured');
+
+var message = {
+      from: "greddy <greddy@163.com>",
+      to: "asd <asd@163.com>",
+      subject: "Hello nodemail!",
+      headers: {
+            'X-Laziness-level': 1000
+      },
+      text: 'Hello nodemail!',
+      html: '<p><b>Hello nodemail！</b></p><img width="100" src="cid:inspectocat@node"/>',
+      attachments: [
+      {
+            fileName: 'string.txt',
+		        contents: 'Some notes about this e-mail',
+		        contentType: 'text/plain'
+		  },
+		  {
+		        fileName: 'notes.txt',
+		        filePath: __dirname + '/attachments/notes.txt',
+	    },
+	    {
+		        fileName: 'image.png',
+		        contents: new Buffer('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' + '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' + 'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC', 'base64'),
+		        cid: 'note@node' // should be as unique as possible
+	    },
+	    {
+		        fileName: 'inspectocat.jpg',
+		        filePath: __dirname + '/attachments/inspectocat.jpg',
+		        cid: 'inspectocat@node' // should be as unique as possible
+	    }]
+}
