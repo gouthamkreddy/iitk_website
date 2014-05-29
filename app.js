fea3bd044c8b59ddc613fc70187093109d918b9c
gouthamkreddy <gouthamkreddy@gmail.com>
2014-05-29 12:18:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-var path
+var path = require('path');
+var express = require('express');
+var config = require('./config');
+var routes = require('./routes');
+
+var app = express.createServer();
+
+app.configure(function() {
+    var viewsRoot = path.join(_dirname, 'views');
+    app.set('views engine', 'html');
+    app.set('views', viewsRoot);
+    app.register('.html', require('ejs'));
+    //app.use(express.bodyParser({}));
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: config.sessionSecret
+    }));
+  });
+  
+  app.helpers({
+      config: config
+  });
+  
+  routes(app);
+  
+  app.listen(config.port);
+  
+  var staticDir = path.join(_dirname, 'public');
+  app.configure('development', function() {
+      app.use(express.static(staticDir));
+      app.usr(express.errorHandler({
+          dumpExceptions: true,
+          showStack: true
+      }));
+  });
+  
+  console.log(config.host + ':' + config.port);
+  module.exports = app;
