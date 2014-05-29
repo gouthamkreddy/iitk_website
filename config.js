2122bf0be8fcddf0509ffe1c903534d691f34ff9
gouthamkreddy <gouthamkreddy@gmail.com>
2014-05-29 12:38:11 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// var path = require('path');
+
+exports.config = {
+	debug: true,
+	name: 'nodemail',
+	description: 'email client',
+	version: 0.1,
+
+	sessionSecret: 'nodemail',
+	authCookieName: 'nodemail',
+	host: '127.0.0.1',
+	port: 3001
+}
